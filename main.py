c66a6e40567dc47a3a43c463851899ae9265f59d
Jojorabbit21 <joegwanghee21@gmail.com>
2021-12-11 23:32:49 +0900

    Major #1211
    Full build complete

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from src.br_uploader import *
 todays_games_url = 'https://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2021/scores/00_todays_scores.json'
 starters_url = 'https://stats.nba.com/js/data/leaders/00_active_starters_{}.json'
 
+team_adv_url = 'https://www.basketball-reference.com/leagues/NBA_2022.html#all_advanced_team'
+
 data_url = 'https://stats.nba.com/stats/leaguedashteamstats?' \
            'Conference=&DateFrom=&DateTo=&Division=&GameScope=&' \
            'GameSegment=&LastNGames=0&LeagueID=00&Location=&' \
@@ -17,7 +19,16 @@ data_url = 'https://stats.nba.com/stats/leaguedashteamstats?' \
            'PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&' \
            'Season=2021-22&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&' \
            'StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision='
-           
+
+trad_url = 'https://stats.nba.com/stats/leaguedashplayerstats?' \
+          'College=&Conference=&Country=&DateFrom=&DateTo=&' \
+          'Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&' \
+          'Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&' \
+          'Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N' \
+          '&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&' \
+          'PlusMinus=N&Rank=N&Season=2021-22&SeasonSegment=&SeasonType=Regular+Season&' \
+          'ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision=&Weight='  
+             
 adv_url = 'https://stats.nba.com/stats/leaguedashplayerstats?' \
           'College=&Conference=&Country=&DateFrom=&DateTo=&' \
           'Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&' \
@@ -49,21 +60,75 @@ data_headers = {
     'x-nba-stats-token': 'true'
 }
 
+team_id = { # NBA.STAT ID TO ABR
+  '1610612737':	'ATL',
+  '1610612738':	'BOS',
+  '1610612751':	'BRK',
+  '1610612766':	'CHO',
+  '1610612741':	'CHI',
+  '1610612739':	'CLE',
+  '1610612742':	'DAL',
+  '1610612743':	'DEN',
+  '1610612765':	'DET',
+  '1610612744':	'GSW',
+  '1610612745':	'HOU',
+  '1610612754':	'IND',
+  '1610612746':	'LAC',
+  '1610612747':	'LAL',
+  '1610612763':	'MEM',
+  '1610612748':	'MIA',
+  '1610612749':	'MIL',
+  '1610612750':	'MIN',
+  '1610612740':	'NOP',
+  '1610612752':	'NYK',
+  '1610612760':	'OKC',
+  '1610612753':	'ORL',
+  '1610612755':	'PHI',
+  '1610612756':	'PHO',
+  '1610612757':	'POR',
+  '1610612758':	'SAC',
+  '1610612759':	'SAN',
+  '1610612761':	'TOR',
+  '1610612762':	'UTA',
+  '1610612764':	'WAS',
+}
+
+def get_player_stats():
+  clear_sheet('STAT','B1:CC')
+  js = get_json_data(trad_url)
+  df = to_data_frame(js)
+  df.sort_values(by=['TEAM_ABBREVIATION'], axis=0, inplace=True)
+  upload_dataframes(df,'STAT',index=False,columns=True)
+
 def get_player_advanced_stats():
-  clear_sheet('STAT')
+  clear_sheet('STAT_A','B1:CC')
   js = get_json_data(adv_url)
   df = to_data_frame(js)
-  return df
+  df.sort_values(by=['TEAM_ABBREVIATION'], axis=0, inplace=True)
+  upload_dataframes(df,'STAT_A',index=False,columns=True)
+
+def get_team_adv():
+  raw = requests.get(team_adv_url)
+  html = raw.text
+  df = pd.read_html(html,match='Advanced')[0]
+  df.columns = df.columns.droplevel()
+  df = df.drop(['Rk','Unnamed: 17_level_1','Unnamed: 22_level_1','Unnamed: 27_level_1','Attend.','Attend./G'], axis=1)
+  df = df.drop(30, axis=0)
+  df.sort_values(by=['Team'],axis=0,inplace=True)
+  clear_sheet('TEAM_A','A1:Y31')
+  upload_dataframes(df,'TEAM_A',index=False, columns=True)
 
 def get_team_stats():
   clear_sheet('TEAM')
   js = get_json_data(data_url)
   df = to_data_frame(js)
-  return df
+  upload_dataframes(df,'TEAM',index=False,columns=True)
 
 def get_json_data(url):
     raw_data = requests.get(url, headers=data_headers)
     json = raw_data.json()
+    with open('pls.json','w') as file:
+      js.dump(json, file)
     return json.get('resultSets')
   
 def get_todays_games_json(url):
@@ -80,16 +145,32 @@ def get_starting_lineups(): #WIP
     td = datetime.now()
     est = td - timedelta(hours=14)
     fd = est.strftime('%Y%m%d')
-    url = starters_url.format('20211209')
-    try:
-      raw_data = requests.get(url, headers=games_header)
-      json = raw_data.json()
-      json = js.loads(json)
-      for key, value in js.loads(json).items:
-        print('key: {} , value: {}'.format(key, value))
-      return json
-    except:
-      print('No data exists')
+    url = starters_url.format('20211210')
+    raw_data = requests.get(url, headers=games_header)
+    json = raw_data.json()
+    keys = json.keys()
+    match_info = []
+    for i in keys:
+      single = []
+      single.append(json[i]['htmid'])
+      single.append(json[i]['vtmid'])
+      if len(json[i]['htm']) > 0:
+        for j in range(0,20):
+          try:
+            single.append(json[i]['htm'][j]['pid'])
+          except:
+            single.append("")
+      if len(json[i]['vtm']) > 0:
+        for j in range(0,20):
+          try:
+            single.append(json[i]['vtm'][j]['pid'])
+          except:
+            single.append("")
+      match_info.append(single)
+    df = pd.DataFrame(data=match_info)
+    clear_sheet('M','A2:AL20')
+    upload_dataframes(df, 'M', index=False, columns=False)
+
  
 def get_player_list():
   URL = 'https://www.basketball-reference.com/players/{}/'
@@ -119,9 +200,8 @@ def get_player_list():
     upload_dataframes(ps_df, 'PMAP')
 
 if __name__ == '__main__':
-  # df = get_player_advanced_stats()
-  # upload_dataframes(df, 'STAT', False, True)
-  # df = get_team_stats()
-  # upload_dataframes(df, 'TEAM',False,True)
+  get_player_stats()
+  get_player_advanced_stats()
   get_starting_lineups()
-
+  get_team_adv()
+  get_team_stats()
--- src/br_uploader.py
+++ src/br_uploader.py
@@ -5,12 +5,18 @@ DB_NAME = 'NBA PPI'
 DB_SHEETS = {
   "PMAP": 'Players',
   "STAT": 'Stats',
-  "TEAM": 'Teams'
+  "STAT_A": 'Stats_Advanced',
+  "TEAM": 'Teams',
+  "TEAM_A": 'Teams_Advanced',
+  'M': 'Starters' 
 }
 DB_START_COL = {
   'PMAP': 2,
-  'STAT': 1,
-  'TEAM': 1
+  'STAT': 2,
+  'STAT_A': 2,
+  'TEAM': 1,
+  "TEAM_A": 1,
+  'M': 1
 }
 
 def upload_dataframes(data, keyword, index=True, columns=True):
@@ -22,7 +28,10 @@ def upload_dataframes(data, keyword, index=True, columns=True):
     DB_START_ROW = 1
   set_with_dataframe(sh, data, row=DB_START_ROW, col=DB_START_COL[keyword], include_index=index, include_column_header=columns)
 
-def clear_sheet(sheetname):
+def clear_sheet(sheetname, range=None):
   gc = gspread.service_account(filename="./src/agent.json")
   sh = gc.open(DB_NAME).worksheet(DB_SHEETS[sheetname])
-  sh.clear()
+  if range is None:
+    sh.clear()
+  else:
+    sh.batch_clear([range])
